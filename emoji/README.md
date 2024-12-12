def512a50879d93a335bbf9e4750f49e15d2e2da
王福元 <wangfuyuan@nnuo.com>
2024-12-12 14:43:44 +0800

    chore: 修改新的 UI

--- emoji/README.md
+++ emoji/README.md
@@ -1,21 +1,13 @@
-<!--
- * @Description: readme
- * @Author: zoeblow
- * @Email: wangfuyuan@nnuo.com
- * @Date: 2020-08-05 17:21:13
- * @LastEditors: zoeblow
- * @LastEditTime: 2020-08-05 17:25:02
- * @FilePath: \emoji\README.md
--->
+# ✨ ❤️ Emoji-Cheat-Sheet Emoji 备忘录 ❤️ ✨
 
-# emoji
+在 GitHub 上发现了这个玩意，由于本人是个聊天不发表情会死星人。Git 提交也不例外，所以就下载了 GitHub 上面的文件部署到Pages上，方便使用。
 
-在 GitHub 上发现了这个玩意，由于本人是个聊天不发表情会死星人。git 提交也不例外，所以就下载了 GitHub 上面的文件部署到码云上，方便使用。
-
-打开页面 [pages](http://zoeblow.gitee.io/emoji)
+打开页面 [pages](https://emoji.maozi.io/)
 
 选择想要的表情点击一下就好了，他会直接进入到你的粘贴板。
 
 ## 版本信息
 
-This page was created by [Arvid Andersson](http://arvidandersson.se) / [@arvid_a](http://twitter.com/arvid_a) at [Oktavilla](http://oktavilla.se). Source code is available on [GitHub](https://GitHub.com/arvida/emoji-cheat-sheet.com).
+根据  <https://github.com/WebpageFX/emoji-cheat-sheet.com> 代码修改而来 如果有侵权请联系我删除，谢谢！
+
+This page was created by [Arvid Andersson](http://arvidandersson.se) . Source code is available on [GitHub](https://GitHub.com/WebpageFX/emoji-cheat-sheet.com).
--- emoji/script/script.js
+++ emoji/script/script.js
@@ -1,9 +1,40 @@
 /*
  * Emoji cheat sheet
  */
-$(document).ready(function() {
+const FX = {};
+FX.version = "0.1.0";
+FX.BackToTop = {
+  $btn: null,
+
+  init() {
+    this.$btn = $(".back-to-top");
+
+    if (this.$btn.length) {
+      this.bind();
+    }
+  },
+
+  bind() {
+    $(window).on("scroll load", this.maybeShowButton.bind(this));
+    this.$btn.on("click", this.scrollToTop);
+  },
+
+  maybeShowButton() {
+    if ($(window).scrollTop() > 500) {
+      this.$btn.fadeIn();
+    } else {
+      this.$btn.fadeOut();
+    }
+  },
+
+  scrollToTop() {
+    $(window).scrollTop(0);
+  },
+};
+
+$(document).ready(function () {
   try {
-    if(document.flashtest && document.flashtest.PercentLoaded()>=0){
+    if (document.flashtest && document.flashtest.PercentLoaded() >= 0) {
       // Flash was able to load the test swf and is working
       initZeroClipboard();
     } else {
@@ -13,37 +44,35 @@ $(document).ready(function() {
     initJsClipboard();
   }
 
-  function initZeroClipboard(){
+  function initZeroClipboard() {
     ZeroClipboard.config({
       forceHandCursor: true,
-      hoverClass: "hover"
+      hoverClass: "hover",
     });
     var clipboardclient = new ZeroClipboard();
 
-    clipboardclient.on('ready', function( readyEvent ) {
-      $('ul').on('mouseover', 'div', function() {
+    clipboardclient.on("ready", function (readyEvent) {
+      $("ul").on("mouseover", "div", function () {
         try {
           clipboardclient.clip(this);
-        } catch(e) { }
+        } catch (e) {}
       });
 
-      clipboardclient.on('copy', function(evt) {
+      clipboardclient.on("copy", function (evt) {
         var clipboard = evt.clipboardData;
         clipboard.setData("text/plain", $(evt.target).text().trim());
       });
 
-      clipboardclient.on('aftercopy', function(evt) {
+      clipboardclient.on("aftercopy", function (evt) {
         var highlightedElement = evt.target;
-        $(highlightedElement).addClass('copied');
-        setTimeout(function(){
-          $(highlightedElement).removeClass('copied');
-        },800);
-
-        _gaq.push(['_trackEvent', 'Emojis', 'Copy', text]);
+        $(highlightedElement).addClass("copied");
+        setTimeout(function () {
+          $(highlightedElement).removeClass("copied");
+        }, 800);
       });
     });
 
-    clipboardclient.on( 'error', function(event) {
+    clipboardclient.on("error", function (event) {
       ZeroClipboard.destroy();
       initJsClipboard();
     });
@@ -51,9 +80,9 @@ $(document).ready(function() {
 
   var jsClipboardSupported = true; // we can't check if this is true unless the user tries once
   function initJsClipboard() {
-    $('ul').on('click', 'div', function() {
+    $("ul").on("click", "div", function () {
       try {
-        if(jsClipboardSupported) {
+        if (jsClipboardSupported) {
           var selection = getSelection();
           selection.removeAllRanges();
 
@@ -62,32 +91,36 @@ $(document).ready(function() {
           selection.addRange(range);
 
           var highlightedElement = $(this);
-          if(document.execCommand('copy')==true) { // this will silently fail on IE11 when access is denied
-            $(highlightedElement).addClass('copied');
-            _gaq.push(['_trackEvent', 'Emojis', 'Copy', $(this).text().trim()]);
-            setTimeout(function(){
-              $(highlightedElement).removeClass('copied');
-            },800);
+          if (document.execCommand("copy") == true) {
+            // this will silently fail on IE11 when access is denied
+            $(highlightedElement).addClass("copied");
+            setTimeout(function () {
+              $(highlightedElement).removeClass("copied");
+            }, 800);
+
+            // console.log(highlightedElement, "highlightedElement");
           } else {
             // copying was not successfull or denied by the user or browser preferences
             // see Firefox about:config "dom.allow_cut_copy"
-            $(highlightedElement).addClass('clipboardError');
-            setTimeout(function(){
-              $(highlightedElement).removeClass('clipboardError');
-            },6000);
+            $(highlightedElement).addClass("clipboardError");
+            setTimeout(function () {
+              $(highlightedElement).removeClass("clipboardError");
+            }, 6000);
 
             jsClipboardSupported = false;
           }
           selection.removeAllRanges();
         }
-      } catch(e) { }
+      } catch (e) {}
     });
   }
 
   function isElementMatching(element, needle) {
     var alternative = element.attr("data-alternative-name");
-    return ($(element).text().toLowerCase().indexOf(needle) >= 0) ||
-      (alternative != null && alternative.toLowerCase().indexOf(needle) >= 0);
+    return (
+      $(element).text().toLowerCase().indexOf(needle) >= 0 ||
+      (alternative != null && alternative.toLowerCase().indexOf(needle) >= 0)
+    );
   }
 
   function highlightElements(needle) {
@@ -97,7 +130,7 @@ $(document).ready(function() {
     }
     needle = needle.toLowerCase();
     $(".emojis li").each(function (index, el) {
-      if (isElementMatching($('.name', el), needle)) {
+      if (isElementMatching($(".name", el), needle)) {
         $(el).show();
       } else {
         $(el).hide();
@@ -109,62 +142,21 @@ $(document).ready(function() {
     $(".emojis li").show();
   }
 
-  $("#header .search").keyup(function(e) {
-    if (e.keyCode == 27) { // ESC
-      $(this).val('').blur();
+  $("#header .search").keyup(function (e) {
+    if (e.keyCode == 27) {
+      // ESC
+      $(this).val("").blur();
       highlightAll();
     }
   });
-  $("#header .search").on("change paste keyup", function() {
+  $("#header .search").on("change paste keyup", function () {
     highlightElements($("#header .search").val());
   });
   $("#header .search").focus();
 
-  var curAudio;
-  var curAudioContainer;
+  FX.BackToTop.init();
+  console.log(FX, "FX");
 
-  function play(e) {
-    e.preventDefault();
-
-    if (curAudio) {
-      curAudio.pause();
-      playStopped();
-    }
-
-    if ($(curAudioContainer).is(this)) {
-      curAudioContainer = null;
-    } else {
-      curAudioContainer = this;
-      soundContainer = $(curAudioContainer).find("~ div").first();
-      soundName = $(soundContainer).data("sound");
-
-      $(curAudioContainer).html('&#x275A;&#x275A; ');
-
-      curAudio = new Audio("https://emoji-cheat-sheet.campfirenow.com/sounds/" + soundName + ".mp3");
-      $(curAudio).on('ended', playStopped);
-      curAudio.play();
-    }
-  }
-
-  function playStopped() {
-    $(curAudioContainer).html('&#9658; ');
-  }
-
-  function canPlayMp3() {
-    var audio = new Audio(),
-      result = audio.canPlayType("audio/mpeg");
-
-    if(result != "") {
-      return true;
-    }
-  }
-
-  if (canPlayMp3() == true) {
-    $("#campfire-sounds li").prepend('<a href="#" class="play">&#9658; </a>');
-    $("#campfire-sounds .play").on("click", play);
-  }
-
-  $("#description a").on("click", function() {
-    _gaq.push(["_trackEvent", "Services", "Click", $(this).text()]);
-  });
+  console.log($("img.lazy").length, "images");
+  $("img.lazy").lazyload();
 });
